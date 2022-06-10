d701556481e3de98994950da09cdd864ed22b22f
seandenielagustin <104070742+seandenielagustin@users.noreply.github.com>
2022-06-10 12:37:19 +0800

    Create agustindatabasebackup.sh

---
+++ agustindatabasebackup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# Set the date format.
+NOW=$(date +"%Y-%m-%d-%H%M")
+
+# MySQL database credentials
+DB_USER="wordpressuser"
+DB_PASS="seanagustin"
+DB_NAME="wordpress"
+DB_FILE="cd /opt/backups"
+
+# create folder in /opt
+mkdir /opt/backups
+
+# going back to cd /opt/backup
+cd /opt/backups
+
+# exporting the wordpress database 
+mysqldump -u root -pSEAN wordpress > wordpress${date}.sql
+
+# compress the wordpress
+gzip -cv wordpress${date}.sql > wordpress${date}.gz 
